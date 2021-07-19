075e6492a0cd45fe6db1edba671fc2094251f2e8
Steven Bennett <steven@srclaunch.com>
2021-07-19 04:59:23 -0700

    Update stuff

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,114 @@
-/******/ (() => { // webpackBootstrap
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
+
+/***/ 9283:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const core_1 = __importDefault(__nccwpck_require__(2186));
+const exec_1 = __importDefault(__nccwpck_require__(1514));
+const github_1 = __importDefault(__nccwpck_require__(5438));
+const glob_1 = __importDefault(__nccwpck_require__(8090));
+function runCommand(command) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let myOutput = '';
+        let myError = '';
+        const options = {
+            cwd: './',
+            listeners: {
+                stderr: (data) => {
+                    myError += data.toString();
+                },
+                stdout: (data) => {
+                    myOutput += data.toString();
+                },
+            },
+        };
+        yield exec_1.default.exec('node', command.split(' '), options);
+        console.log(myError);
+        console.log(myOutput);
+    });
+}
+function main() {
+    var e_1, _a;
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const projectId = core_1.default.getInput('srclaunch-project-id');
+            const pipelineSecret = core_1.default.getInput('srclaunch-project-pipeline-secret');
+            const githubToken = core_1.default.getInput('github-token');
+            const context = github_1.default.context;
+            const repo = context.repo;
+            const pushPayload = github_1.default.context.payload;
+            const headCommit = pushPayload.head_commit;
+            const octokit = github_1.default.getOctokit(githubToken);
+            const globber = yield glob_1.default.create('entities/*.model.js');
+            try {
+                for (var _b = __asyncValues(globber.globGenerator()), _c; _c = yield _b.next(), !_c.done;) {
+                    const file = _c.value;
+                    console.log(file);
+                }
+            }
+            catch (e_1_1) { e_1 = { error: e_1_1 }; }
+            finally {
+                try {
+                    if (_c && !_c.done && (_a = _b.return)) yield _a.call(_b);
+                }
+                finally { if (e_1) throw e_1.error; }
+            }
+            // await runCommand('ls -la');
+            // octokit.rest.repos.downloadTarballArchive({
+            //   owner: repo.owner,
+            //   ref: context.ref,
+            //   repo: repo.repo,
+            // });
+            // You can also pass in additional options as a second parameter to getOctokit
+            // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
+            // const { data: pullRequest } = await octokit.rest.pulls.get({
+            //   mediaType: {
+            //     format: 'diff',
+            //   },
+            //   owner: 'octokit',
+            //   pull_number: 123,
+            //   repo: 'rest.js',
+            // });
+            // console.log(`Hello ${nameToGreet}!`);
+            // const time = new Date().toTimeString();
+            // core.setOutput('time', time);
+            // Get the JSON webhook payload for the event that triggered the workflow
+            const payload = JSON.stringify(github_1.default.context.payload, undefined, 2);
+            console.log(`The event payload: ${payload}`);
+        }
+        catch (error) {
+            console.log('error', error);
+            // core.setFailed(error.message);
+        }
+    });
+}
+main();
+
+
+/***/ }),
 
 /***/ 7351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
@@ -9993,205 +10102,18 @@ module.exports = require("zlib");
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__nccwpck_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__nccwpck_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(2186);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__nccwpck_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _actions_exec__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(1514);
-/* harmony import */ var _actions_exec__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__nccwpck_require__.n(_actions_exec__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_2__ = __nccwpck_require__(5438);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__nccwpck_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _actions_glob__WEBPACK_IMPORTED_MODULE_3__ = __nccwpck_require__(8090);
-/* harmony import */ var _actions_glob__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__nccwpck_require__.n(_actions_glob__WEBPACK_IMPORTED_MODULE_3__);
-var __awaiter = (undefined && undefined.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (undefined && undefined.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-var __asyncValues = (undefined && undefined.__asyncValues) || function (o) {
-    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
-    var m = o[Symbol.asyncIterator], i;
-    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
-    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
-    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
-};
-
-
-
-
-function runCommand(command) {
-    return __awaiter(this, void 0, void 0, function () {
-        var myOutput, myError, options;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    myOutput = '';
-                    myError = '';
-                    options = {
-                        cwd: './',
-                        listeners: {
-                            stderr: function (data) {
-                                myError += data.toString();
-                            },
-                            stdout: function (data) {
-                                myOutput += data.toString();
-                            },
-                        },
-                    };
-                    return [4 /*yield*/, exec.exec('node', command.split(' '), options)];
-                case 1:
-                    _a.sent();
-                    console.log(myError);
-                    console.log(myOutput);
-                    return [2 /*return*/];
-            }
-        });
-    });
-}
-function main() {
-    var e_1, _a;
-    return __awaiter(this, void 0, void 0, function () {
-        var projectId, pipelineSecret, githubToken, context, repo, pushPayload, headCommit, octokit, globber, _b, _c, file, e_1_1, payload, error_1;
-        return __generator(this, function (_d) {
-            switch (_d.label) {
-                case 0:
-                    _d.trys.push([0, 14, , 15]);
-                    projectId = _actions_core__WEBPACK_IMPORTED_MODULE_0___default().getInput('srclaunch-project-id');
-                    pipelineSecret = _actions_core__WEBPACK_IMPORTED_MODULE_0___default().getInput('srclaunch-project-pipeline-secret');
-                    githubToken = _actions_core__WEBPACK_IMPORTED_MODULE_0___default().getInput('github-token');
-                    context = (_actions_github__WEBPACK_IMPORTED_MODULE_2___default().context);
-                    repo = context.repo;
-                    pushPayload = (_actions_github__WEBPACK_IMPORTED_MODULE_2___default().context.payload);
-                    headCommit = pushPayload.head_commit;
-                    octokit = _actions_github__WEBPACK_IMPORTED_MODULE_2___default().getOctokit(githubToken);
-                    return [4 /*yield*/, _actions_glob__WEBPACK_IMPORTED_MODULE_3___default().create('entities/*.model.js')];
-                case 1:
-                    globber = _d.sent();
-                    _d.label = 2;
-                case 2:
-                    _d.trys.push([2, 7, 8, 13]);
-                    _b = __asyncValues(globber.globGenerator());
-                    _d.label = 3;
-                case 3: return [4 /*yield*/, _b.next()];
-                case 4:
-                    if (!(_c = _d.sent(), !_c.done)) return [3 /*break*/, 6];
-                    file = _c.value;
-                    console.log(file);
-                    _d.label = 5;
-                case 5: return [3 /*break*/, 3];
-                case 6: return [3 /*break*/, 13];
-                case 7:
-                    e_1_1 = _d.sent();
-                    e_1 = { error: e_1_1 };
-                    return [3 /*break*/, 13];
-                case 8:
-                    _d.trys.push([8, , 11, 12]);
-                    if (!(_c && !_c.done && (_a = _b.return))) return [3 /*break*/, 10];
-                    return [4 /*yield*/, _a.call(_b)];
-                case 9:
-                    _d.sent();
-                    _d.label = 10;
-                case 10: return [3 /*break*/, 12];
-                case 11:
-                    if (e_1) throw e_1.error;
-                    return [7 /*endfinally*/];
-                case 12: return [7 /*endfinally*/];
-                case 13:
-                    payload = JSON.stringify((_actions_github__WEBPACK_IMPORTED_MODULE_2___default().context.payload), undefined, 2);
-                    console.log("The event payload: " + payload);
-                    return [3 /*break*/, 15];
-                case 14:
-                    error_1 = _d.sent();
-                    console.log('error', error_1);
-                    return [3 /*break*/, 15];
-                case 15: return [2 /*return*/];
-            }
-        });
-    });
-}
-main();
-
-})();
-
-module.exports = __webpack_exports__;
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __nccwpck_require__(9283);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
 /******/ })()
 ;
+//# sourceMappingURL=index.js.map
